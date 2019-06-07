a614ee00b498cc78e2a84dc99bab99749ebb30d4
alex.litvinovskiy <alexlitvinovskiy@MacBook-Pro-Alex.local>
2019-06-07 07:25:08 +0200

    initial commit

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ $(function(){
 	});
 
 });
+
+$(document).ready(function(){
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(200);
+     }, 4000);
+    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+        $("#cookieConsent").fadeOut(200);
+    });
+});
